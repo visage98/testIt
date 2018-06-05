09551e82fdb8f89987e364ab77f83dda44a86b1c
Deepanshu Bagri <deepanshu.bagri98@gmail.com>
2018-06-05 15:11:13 +0530

    Heroku Deploy

--- config/config.js
+++ config/config.js
@@ -7,6 +7,6 @@ module.exports = {
         clientID        : process.env.FACEBOOK_ID || '1620457874720007',
         clientSecret    : process.env.FACEBOOK_SECRET || 'a312ba65f5c9f680d6b04366474b4157',
         profileFields   : ['emails','displayName'],
-        callbackURL     : '/auth/facebook/callback'
+        callbackURL     : 'https://testitonline.herokuapp.com/auth/facebook/callback'
     }
 }
